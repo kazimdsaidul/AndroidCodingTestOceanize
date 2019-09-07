f1033b43cf71ae949fa14df32dd5ad8d7cdbaa73
Kazi Md. Saidul <kazimdsaidul@gmail.com>
2019-09-07 16:55:31 +0600

    added nav item and searchview

--- app/src/main/java/com/kazi/test/MainActivity.kt
+++ app/src/main/java/com/kazi/test/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.kazi.test
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var searchView: SearchView
+    private var isShowSearchView = false;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +30,76 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_employee_list, R.id.navigation_search, R.id.navigation_create
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        navView.setOnNavigationItemSelectedListener { item ->
+
+            invalidateOptionsMenu();
+
+            when (item.itemId) {
+                R.id.navigation_employee_list -> {
+                    isShowSearchView = false
+                }
+
+                R.id.navigation_search -> {
+                    Log.e("", "")
+                    isShowSearchView = true
+                }
+
+                R.id.navigation_create -> {
+                    isShowSearchView = false
+                }
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
+
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        // Associate searchable configuration with the SearchView
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchView = menu.findItem(R.id.action_search)
+            .getActionView() as SearchView
+        searchView.setSearchableInfo(
+            searchManager
+                .getSearchableInfo(componentName)
+        )
+        searchView.setMaxWidth(Integer.MAX_VALUE)
+
+        // listening to search query text change
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                // filter recycler view when query submitted
+                //mAdapter.getFilter().filter(query)
+                return false
+            }
+
+            override fun onQueryTextChange(query: String): Boolean {
+                // filter recycler view when text is changed
+                //  mAdapter.getFilter().filter(query)
+                return false
+            }
+        })
+
+        if (isShowSearchView) {
+
+            searchView.visibility = View.VISIBLE
+            searchView.onActionViewExpanded()
+            return true
+        } else {
+            searchView.visibility = View.GONE
+
+            return false
+        }
+
+    }
+
+
 }
